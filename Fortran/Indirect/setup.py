68735146ed96786645f9c835c1e07b58ebf98736
Stephen Smith <stephen.smith@stfc.ac.uk>
2021-09-14 13:51:37 +0100

    Linux doesn't like the static flags

--- Fortran/Indirect/setup.py
+++ Fortran/Indirect/setup.py
@@ -4,9 +4,7 @@ import numpy.distutils.core
 
 ResNorm = numpy.distutils.core.Extension(name="ResNorm", sources=['mantidindirect/Bayes/ResNorm_main.f90', 'mantidindirect/Bayes/ResNorm_subs.f90',
                                                                   'mantidindirect/Bayes/BlrRes.f90', 'mantidindirect/Bayes/Bayes.f90',
-                                                                  'mantidindirect/Bayes/Four.f90', 'mantidindirect/Bayes/Util.f90'],
-                                         extra_link_args=["-static", "-static-libgfortran", "-static-libgcc"])
-
+                                                                  'mantidindirect/Bayes/Four.f90', 'mantidindirect/Bayes/Util.f90'])
 Quest = numpy.distutils.core.Extension(name="Quest",
                                        sources=['mantidindirect/Bayes/Quest_main.f90',
                                                 'mantidindirect/Bayes/Quest_subs.f90',
@@ -14,9 +12,7 @@ Quest = numpy.distutils.core.Extension(name="Quest",
                                                 'mantidindirect/Bayes/Bayes.f90',
                                                 'mantidindirect/Bayes/Four.f90',
                                                 'mantidindirect/Bayes/Util.f90',
-                                                'mantidindirect/Bayes/Simopt.f90'],
-                                       extra_link_args=["-static", "-static-libgfortran", "-static-libgcc"])
-
+                                                'mantidindirect/Bayes/Simopt.f90'])
 QLse = numpy.distutils.core.Extension(name="QLse",
                                       sources=['mantidindirect/Bayes/QLse_main.f90',
                                                'mantidindirect/Bayes/QLse_subs.f90',
@@ -24,33 +20,25 @@ QLse = numpy.distutils.core.Extension(name="QLse",
                                                'mantidindirect/Bayes/Bayes.f90',
                                                'mantidindirect/Bayes/Four.f90',
                                                'mantidindirect/Bayes/Util.f90',
-                                               'mantidindirect/Bayes/Simopt.f90'],
-                                      extra_link_args=["-static", "-static-libgfortran", "-static-libgcc"])
-
+                                               'mantidindirect/Bayes/Simopt.f90'])
 QLres = numpy.distutils.core.Extension(name="QLres",
                                        sources=['mantidindirect/Bayes/QLres_main.f90',
                                                 'mantidindirect/Bayes/QLres_subs.f90',
                                                 'mantidindirect/Bayes/BlrRes.f90',
                                                 'mantidindirect/Bayes/Bayes.f90',
                                                 'mantidindirect/Bayes/Four.f90',
-                                                'mantidindirect/Bayes/Util.f90'],
-                                       extra_link_args=["-static", "-static-libgfortran", "-static-libgcc"])
-
+                                                'mantidindirect/Bayes/Util.f90'])
 
 QLdata = numpy.distutils.core.Extension(name="QLdata",
                                         sources=['mantidindirect/Bayes/QLdata_main.f90',
                                                  'mantidindirect/Bayes/QLdata_subs.f90',
                                                  'mantidindirect/Bayes/Bayes.f90',
                                                  'mantidindirect/Bayes/Four.f90',
-                                                 'mantidindirect/Bayes/Util.f90'],
-                                        extra_link_args=["-static", "-static-libgfortran", "-static-libgcc"])
-
+                                                 'mantidindirect/Bayes/Util.f90'])
 muscat = numpy.distutils.core.Extension(name="muscat",
                                         sources=['mantidindirect/AbsCorrection/muscat_data_main.f90',
                                                  'mantidindirect/AbsCorrection/muscat_data.f90',
-                                                 'mantidindirect/AbsCorrection/muscat_geom.f90'],
-                                        extra_link_args=["-static", "-static-libgfortran", "-static-libgcc"])
-
+                                                 'mantidindirect/AbsCorrection/muscat_geom.f90'])
 numpy.distutils.core.setup(
     name='mantidindirect',
     ext_modules=[ResNorm, Quest, QLse, QLres, QLdata, muscat],
@@ -60,4 +48,4 @@ numpy.distutils.core.setup(
     url='https://www.mantidproject.org',
     version="0.1.0",
     license='BSD',
-    )
+)
