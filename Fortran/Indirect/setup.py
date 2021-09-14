e6e1143539c3823bb781889d71d60a2a3c97f912
Stephen Smith <stephen.smith@stfc.ac.uk>
2021-09-14 13:22:49 +0100

    Add setup.py support

---
+++ Fortran/Indirect/setup.py
@@ -0,0 +1,63 @@
+import setuptools
+import numpy.distutils.core
+
+
+ResNorm = numpy.distutils.core.Extension(name="ResNorm", sources=['mantidindirect/Bayes/ResNorm_main.f90', 'mantidindirect/Bayes/ResNorm_subs.f90',
+                                                                  'mantidindirect/Bayes/BlrRes.f90', 'mantidindirect/Bayes/Bayes.f90',
+                                                                  'mantidindirect/Bayes/Four.f90', 'mantidindirect/Bayes/Util.f90'],
+                                         extra_link_args=["-static", "-static-libgfortran", "-static-libgcc"])
+
+Quest = numpy.distutils.core.Extension(name="Quest",
+                                       sources=['mantidindirect/Bayes/Quest_main.f90',
+                                                'mantidindirect/Bayes/Quest_subs.f90',
+                                                'mantidindirect/Bayes/BlrRes.f90',
+                                                'mantidindirect/Bayes/Bayes.f90',
+                                                'mantidindirect/Bayes/Four.f90',
+                                                'mantidindirect/Bayes/Util.f90',
+                                                'mantidindirect/Bayes/Simopt.f90'],
+                                       extra_link_args=["-static", "-static-libgfortran", "-static-libgcc"])
+
+QLse = numpy.distutils.core.Extension(name="QLse",
+                                      sources=['mantidindirect/Bayes/QLse_main.f90',
+                                               'mantidindirect/Bayes/QLse_subs.f90',
+                                               'mantidindirect/Bayes/BlrRes.f90',
+                                               'mantidindirect/Bayes/Bayes.f90',
+                                               'mantidindirect/Bayes/Four.f90',
+                                               'mantidindirect/Bayes/Util.f90',
+                                               'mantidindirect/Bayes/Simopt.f90'],
+                                      extra_link_args=["-static", "-static-libgfortran", "-static-libgcc"])
+
+QLres = numpy.distutils.core.Extension(name="QLres",
+                                       sources=['mantidindirect/Bayes/QLres_main.f90',
+                                                'mantidindirect/Bayes/QLres_subs.f90',
+                                                'mantidindirect/Bayes/BlrRes.f90',
+                                                'mantidindirect/Bayes/Bayes.f90',
+                                                'mantidindirect/Bayes/Four.f90',
+                                                'mantidindirect/Bayes/Util.f90'],
+                                       extra_link_args=["-static", "-static-libgfortran", "-static-libgcc"])
+
+
+QLdata = numpy.distutils.core.Extension(name="QLdata",
+                                        sources=['mantidindirect/Bayes/QLdata_main.f90',
+                                                 'mantidindirect/Bayes/QLdata_subs.f90',
+                                                 'mantidindirect/Bayes/Bayes.f90',
+                                                 'mantidindirect/Bayes/Four.f90',
+                                                 'mantidindirect/Bayes/Util.f90'],
+                                        extra_link_args=["-static", "-static-libgfortran", "-static-libgcc"])
+
+muscat = numpy.distutils.core.Extension(name="muscat",
+                                        sources=['mantidindirect/AbsCorrection/muscat_data_main.f90',
+                                                 'mantidindirect/AbsCorrection/muscat_data.f90',
+                                                 'mantidindirect/AbsCorrection/muscat_geom.f90'],
+                                        extra_link_args=["-static", "-static-libgfortran", "-static-libgcc"])
+
+numpy.distutils.core.setup(
+    name='mantidindirect',
+    ext_modules=[ResNorm, Quest, QLse, QLres, QLdata, muscat],
+    description='Mantid indirect fortran libraries for Bayes and Absorption correction analysis',
+    author='Mantid Team',
+    author_email="mantid-help@mantidproject.org",
+    url='https://www.mantidproject.org',
+    version="0.1.0",
+    license='BSD',
+    )
