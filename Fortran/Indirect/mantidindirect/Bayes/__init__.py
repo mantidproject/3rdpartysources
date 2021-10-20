3784a819e3f9a3031ce5ed0985f3c3c5ee4e46ea
Stephen <stephen.smith@stfc.ac.uk>
2021-10-20 12:58:59 +0100

    Building static lib extensions on windows

---
+++ Fortran/Indirect/mantidindirect/Bayes/__init__.py
@@ -0,0 +1,10 @@
+# Copyright &copy; 2021 ISIS Rutherford Appleton Laboratory UKRI,
+#   NScD Oak Ridge National Laboratory, European Spallation Source,
+#   Institut Laue - Langevin & CSNS, Institute of High Energy Physics, CAS
+# SPDX - License - Identifier: BSD-3-Clause
+
+# --- Public API --------------------------------------------------------------
+from . import QLdata, QLres, muscat, ResNorm, QLse
+
+
+
